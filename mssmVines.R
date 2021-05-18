3662122f30c158d98aa16aee9cb043fb7676d5fc
katherine anarde laptop <UNCblahblahblah19!>
2021-05-18 18:35:09 -0400

    clean up comments

--- mssmVines.R
+++ mssmVines.R
@@ -16,7 +16,7 @@
 #
 # ------------------------------user input------------------------------
 
-setwd("/Users/KatherineAnardeWheels/PyCharmProjects/Barrier3D/Tools/Multivariate_Sea_Storm_Model") 
+# setwd("/Users/KatherineAnardeWheels/PyCharmProjects/Barrier3D/Tools/Multivariate_Sea_Storm_Model")
 
 # load R packages
 library(CDVine)
@@ -28,9 +28,6 @@ U <- read.table("U_mssm.txt")   # N x d data matrix (with uniform margins)
 nSimNum <- read.table("Inputs_mssm.txt")   # 10000
 
 # ------------------------------model-------------------------------
-
-# from MultiHazard toolbox:
-# Standard trivariate copulas lack flexibility to model joint distributions where heterogeneous dependencies exist between the variable pairs. Pair copula constructions construct multivariate distribution using a cascade of bivariate copulas (some of which are conditional). As the dimensionality of the problem increases the number of mathematically equally valid decompositions quickly becomes large. Bedford and Cooke (2001,2002) introduced the regular vine, a graphical model which helps to organize the possible decompositions. The Canonical (C-) and D- vine are two commonly utilized sub-categories of regular vines, in the trivariate case a vine copula is simultaneously a C- and D-vine. Lets fit a regular vine copula model
 
 # copula model parameters
 type  <- "DVine";          # options are "CVine" or "DVine"
